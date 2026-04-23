0283841cbe1ac7f94368f4c25823fc897e59cbd0
Ryan Morrow <Rylion9@gmail.com>
2026-04-22 21:24:53 -0600

    clean repo and add quarto site

--- national_parks/__init__.py
+++ national_parks/__init__.py
@@ -1,9 +1,9 @@
-from .data import get_parks
+from .data import get_parks_data
 from .clean import clean_parks
 from .analyze import summarize_parks, top_parks_by_alerts
 
 __all__ = [
-    "get_parks",
+    "get_parks_data",
     "clean_parks",
     "summarize_parks",
     "top_parks_by_alerts",
